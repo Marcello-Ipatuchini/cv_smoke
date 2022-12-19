279f856d55e35a9165172ab0365fb27a7f3d9252
Mark Ipatov <mark.ipatov@autobidmaster.com>
2022-12-19 18:39:00 +0300

    - minor selector refactoring

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,24 +1,43 @@
 from .base_page import BasePage
-from randomizer import randomEmail
+from randomizer import randomEmail, randomName
 from selenium.webdriver.common.by import By
 
+
 class MainPageLocators:
-
-        MY_ACCOUNT_BTN = By.CSS_SELECTOR, "[aria-label='My Account']"
-        LOGIN_BTN = By.CSS_SELECTOR, "[type='submit']"
-        EMAIL_INPUT = By.ID, "email-input"
-        PASSWORD_INPUT = By.CSS_SELECTOR, "[name='password']"
-        VIN_INPUT = By.CSS_SELECTOR, "[name='vin']"
-        ORDER_BTN = By.XPATH, "//div/form/button"
-
+    MY_ACCOUNT_BTN = By.CSS_SELECTOR, "[aria-label='My Account']"
+    LOGIN_BTN = By.CSS_SELECTOR, "[type='submit']"
+    EMAIL_INPUT = By.CSS_SELECTOR, '[name="email"]'
+    PASSWORD_INPUT = By.CSS_SELECTOR, "[name='password']"
+    VIN_INPUT = By.CSS_SELECTOR, "[name='vin']"
+    ORDER_BTN = By.XPATH, "//div/form/button"
+    REGISTER_BUTTON = By.CSS_SELECTOR, 'a[href="/en/user/register"]'
+    FIRST_NAME = By.ID, "firstName"
 
 class MainPageActions(BasePage):
     def login_btn_click(self):
         login_button = self.find_element(MainPageLocators.LOGIN_BTN)
         login_button.click()
-    def email_input_random(self):
-        input_email = self.find_element(MainPageLocators.EMAIL_INPUT)
-        input_email.send_keys(randomEmail)
+
     def order_btn_click(self):
         order_button = self.find_element(MainPageLocators.ORDER_BTN)
         order_button.click()
+
+    def email_input_random(self):
+        input_email = self.find_element(MainPageLocators.EMAIL_INPUT)
+        input_email.send_keys(randomEmail)
+
+    def email_input_hardcoded(self):
+        input_email = self.find_element(MainPageLocators.EMAIL_INPUT)
+        input_email.send_keys("79818059334z+2@gmail.com")
+
+    def password_input_hardcoded(self):
+        password_input = self.find_element(MainPageLocators.PASSWORD_INPUT)
+        password_input.send_keys("010101@@@Aa")
+
+    def register_btn_click(self):
+        register_btn = self.find_element(MainPageLocators.REGISTER_BUTTON)
+        register_btn.click()
+
+    def first_name_random(self):
+        input_email = self.find_element(MainPageLocators.FIRST_NAME)
+        input_email.send_keys(randomName)
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -3,10 +3,20 @@ from pages.base_page import BasePage
 from pages.main_page import MainPageActions
 
 
-
 class TestUserSmokeTests:
     @pytest.fixture(autouse=True)
-    def user_login(self, setup):
-        self.user_login = MainPageActions(setup)
-    def test_click(self):
-        self.user_login.login_btn_click()
+    def browser_setup(self, setup):
+        self.browser_setup = MainPageActions(setup)
+
+    def test_user_login(self):
+        self.browser_setup.email_input_hardcoded()
+        self.browser_setup.password_input_hardcoded()
+        self.browser_setup.login_btn_click()
+
+
+    def test_user_registration(self):
+        self.browser_setup.email_input_random()
+        self.browser_setup.login_btn_click()
+        self.browser_setup.register_btn_click()
+        self.browser_setup.first_name_random()
+        self.browser_setup.email_input_random()
